6d6c885d09b9dd38a307a28ca892ea11173f6bad
Diego Jimenez <diego.jimenez@promidat.com>
2019-02-05 15:28:59 -0600

    Corrección Generación reporte

--- inst/application/server.R
+++ inst/application/server.R
@@ -150,8 +150,6 @@ shinyServer(function(input, output, session) {
     nombre.datos <<- paste0(nombre.datos, paste(rep.vars, collapse = "."))
     datos.reporte[[nombre.datos]] <<- datos
     init.replist(nombre.datos)
-    createLogBasico(nombre.datos, "Transformacion de los Datos",
-                    paste0(code.res, "\nstr(datos)"))
     shiny::isolate(eval(parse(text = code.res)))
     shinyAce::updateAceEditor(session, "fieldCodeTrans", value = code.res)
     updateData$datos <- datos
